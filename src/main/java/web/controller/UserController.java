605eb6dcdc595bbe4b42f20164d66aa638be3b3a
Homval <in00@list.ru>
2024-07-17 09:47:05 +0300

    final commit

--- src/main/java/web/controller/UserController.java
+++ src/main/java/web/controller/UserController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import web.entity.User;
 import web.service.UserService;
@@ -26,6 +28,31 @@ public class UserController {
     @GetMapping("/delete")
     public String deleteUser(@RequestParam(name = "id") long id) {
         userService.deleteUser(id);
+        return "redirect:/";
+    }
+
+    @GetMapping("/add")
+    public String createUser(Model model) {
+        model.addAttribute("user", new User());
+        return "add_user";
+    }
+
+    @PostMapping("/add")
+    public String addUser(@ModelAttribute("user") User user) {
+        userService.createUser(user);
+        return "redirect:/";
+    }
+
+    @GetMapping("/edit")
+    public String editUser(@RequestParam(name = "id") long id, Model model) {
+        User user = userService.getUserById(id);
+        model.addAttribute("user", user);
+        return "edit_user";
+    }
+
+    @PostMapping("/edit")
+    public String updateUser(@ModelAttribute("user") User user) {
+        userService.updateUser(user);
         return "redirect:/";
     }
 }
